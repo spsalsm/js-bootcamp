a3fa3ef963a841bf158c068e3dd28e2d863656ef
Sean Salsman <sean.salsman@jbhunt.com>
2019-11-01 10:31:33 -0500

    Lesson 17 complete

---
+++ basics/boolean.js
@@ -0,0 +1,23 @@
+// === - equality operator
+// !== - not equal operator
+// < - less than operator
+// > - greater than operator
+
+let temp = 32
+let isFreezing = temp <= 32
+
+console.log(isFreezing)
+
+// Challenge area
+
+// Create age variable set to your age
+// Calculate isChild - 7 or under
+// Calculate isSenior - 65 or older
+// Print isChild and isSenior values
+let age = 32
+
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log('isChild? ' + isChild)
+console.log('isSenior? ' + isSenior)
